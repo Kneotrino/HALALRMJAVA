e1fb312273cd673555bfcc17ddfa31c3597bebf6
NeutrinoSae <kneotrino@yahoo.com>
2019-05-15 17:33:15 +0800

    JAVA

--- app/src/main/java/com/clay/halalrm/RumahMakanActivity.java
+++ app/src/main/java/com/clay/halalrm/RumahMakanActivity.java
@@ -1,14 +1,21 @@
 package com.clay.halalrm;
 
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.clay.halalrm.fragment.DaftarMenuFragment;
@@ -17,6 +24,9 @@ import com.clay.halalrm.fragment.MainFragment;
 import com.clay.halalrm.fragment.dummy.DummyContent;
 import com.clay.halalrm.model.DaftarMenu;
 import com.clay.halalrm.model.RumahMakan;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class RumahMakanActivity extends AppCompatActivity
     implements InfoFragment.OnFragmentInteractionListener, DaftarMenuFragment.OnListFragmentInteractionListener
@@ -41,8 +51,7 @@ public class RumahMakanActivity extends AppCompatActivity
     };
 
     private void switchToInfo() {
-
-        System.out.println("rumahMakan = " + rumahMakan);
+        btnAddMenu.setVisibility(View.GONE);
         FragmentTransaction fragmentTransaction = RumahMakanActivity.this.getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.RumahMakanFrame, InfoFragment.newInstance(AdminMode,idRM));
         fragmentTransaction.commit();
@@ -50,22 +59,29 @@ public class RumahMakanActivity extends AppCompatActivity
 
 
     private void switchToMenu() {
+
+        if (!AdminMode)
+            btnAddMenu.setVisibility(View.GONE);
+        else
+            btnAddMenu.setVisibility(View.VISIBLE);
         FragmentTransaction fragmentTransaction = RumahMakanActivity.this.getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.RumahMakanFrame,DaftarMenuFragment.newInstance(AdminMode,idRM));
         fragmentTransaction.commit();
-
-
     }
 
     boolean AdminMode;
     RumahMakan rumahMakan;
     long idRM;
+
+    Button btnAddMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rumah_makan);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
+        btnAddMenu = findViewById(R.id.btnAddMenu);
+
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         SetupView();
         switchToInfo();
@@ -81,8 +97,82 @@ public class RumahMakanActivity extends AppCompatActivity
         rumahMakan = RumahMakan.findById(RumahMakan.class,idRM);
         setTitle(rumahMakan.getName());
 
+        btnAddMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddMenu();
+            }
+        });
     }
 
+    private void AddMenu() {
+        LayoutInflater li = LayoutInflater.from(RumahMakanActivity.this);
+        View prompt = li.inflate(R.layout.form_add_menu, null);
+
+        final EditText txtNamaMenu = prompt.findViewById(R.id.txtNamaMenu);
+        final EditText txtHargaMenu = prompt.findViewById(R.id.txtHargaMenu);
+        final List<EditText> editTextList = new LinkedList<>();
+        editTextList.add(txtNamaMenu);
+        editTextList.add(txtNamaMenu);
+
+
+        final AlertDialog dialog = new AlertDialog.Builder(RumahMakanActivity.this)
+                .setView(prompt)
+                .setTitle("Admin Mode Input Data Penguna")
+                .setMessage("Masukan Data Penguna Baru")
+                .setPositiveButton(android.R.string.ok, null) //Set to null. We override the onclick
+                .setNegativeButton(android.R.string.cancel, null)
+                .create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+
+                Button btnSimpan = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                Button btnBatal = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_NEGATIVE);
+
+
+                btnBatal.setText("Batal");
+                btnSimpan.setText("Tambah");
+                btnSimpan.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        boolean inputCheck = checkEditText(editTextList);
+                        System.out.println("inputCheck = " + inputCheck);
+                        if (inputCheck)    {
+                            DaftarMenu daftarMenu =
+                                    new DaftarMenu(
+                                            txtNamaMenu.getText().toString(),
+                                            txtHargaMenu.getText().toString(),
+                                            rumahMakan.getId()
+                                    );
+                            System.out.println("Add.daftarMenu = " + daftarMenu);
+                            daftarMenu.save();
+                            dialog.dismiss();
+                            switchToMenu();
+                        }
+                    }
+                });
+            }
+        });
+        dialog.show();
+    }
+
+    private boolean checkEditText(List<EditText> editTextList) {
+
+        for (EditText editText: editTextList) {
+            if (TextUtils.isEmpty(editText.getText().toString())) {
+                editText.setError("Harus isi");
+                return false;
+            }
+            if ( editText.getText().toString().length() < 4) {
+                editText.setError("Input minimal 4 karakter");
+                return false;
+            }
+        }
+        return true;
+    }
     @Override
     public void onFragmentInteraction(Uri uri) {
 
@@ -91,6 +181,72 @@ public class RumahMakanActivity extends AppCompatActivity
     @Override
     public void onListFragmentInteraction(DaftarMenu item) {
         System.out.println("item = " + item);
+        if (AdminMode)
+            EditMenu(item);
+    }
+
+    private void EditMenu(final DaftarMenu item) {
+        LayoutInflater li = LayoutInflater.from(RumahMakanActivity.this);
+        View prompt = li.inflate(R.layout.form_add_menu, null);
+
+        final EditText txtNamaMenu = prompt.findViewById(R.id.txtNamaMenu);
+        final EditText txtHargaMenu = prompt.findViewById(R.id.txtHargaMenu);
+        final List<EditText> editTextList = new LinkedList<>();
+        editTextList.add(txtNamaMenu);
+        editTextList.add(txtNamaMenu);
+
+        txtNamaMenu.setText(item.getHarga());
+        txtHargaMenu.setText(item.getNama());
+
+
+        final AlertDialog dialog = new AlertDialog.Builder(RumahMakanActivity.this)
+                .setView(prompt)
+                .setTitle("Admin Mode Input Data Penguna")
+                .setMessage("Masukan Data Penguna Baru")
+                .setPositiveButton(android.R.string.ok, null) //Set to null. We override the onclick
+                .setNegativeButton(android.R.string.cancel, null)
+                .setNeutralButton("Batal", null)
+                .create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+
+                Button btnSimpan = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                Button btnBatal = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_NEGATIVE);
+
+
+                btnBatal.setText("Hapus");
+                btnSimpan.setText("Edit");
+
+                btnSimpan.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean inputCheck = checkEditText(editTextList);
+                        System.out.println("inputCheck = " + inputCheck);
+                        if (inputCheck)    {
+                            item.setHarga(txtNamaMenu.getText().toString());
+                            item.setNama(txtHargaMenu.getText().toString());
+                            System.out.println("Update.daftarMenu = " + item);
+                            item.save();
+                            dialog.dismiss();
+                            switchToMenu();
+                        }
+                    }
+                });
+                btnBatal.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                            System.out.println("delete.daftarMenu = " + item);
+                            item.delete();
+                            dialog.dismiss();
+                            switchToMenu();
+                    }
+                });
+
+            }
+        });
+        dialog.show();
 
     }
 }
